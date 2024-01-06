cca54d77eb6f9de701e8d807849d07ec521cc8b2
abirmehedi77 <86663285+abirmehedi77@users.noreply.github.com>
2024-01-07 00:57:37 +0800

    Update users.blade.php

--- resources/views/admin/components/contents/users.blade.php
+++ resources/views/admin/components/contents/users.blade.php
@@ -565,39 +565,4 @@
                 }
             })
         </script>
-        {{-- // notification --}}
-    {{-- @if (session()->has('notification'))
-    <script>
-        
-        $(document).ready(function() {
-           
-            // Set Toastr options
-            toastr.options = {
-                "closeButton": false,
-                "debug": false,
-                "newestOnTop": false,
-                "progressBar": false,
-                "positionClass": "toast-top-right",
-                "preventDuplicates": false,
-                "onclick": null,
-                "showDuration": 300,
-                "hideDuration": 1000,
-                "timeOut": 5000,
-                "extendedTimeOut": 1000,
-                "showEasing": "swing",
-                "hideEasing": "linear",
-                "showMethod": "fadeIn",
-                "hideMethod": "fadeOut"
-            };
-            var notificationJson = {!! json_encode(session('notification')) !!};
-            var notification = JSON.parse(notificationJson);
-            console.log(notification.fields.email)
-            toastr[notification.status](notification.message);
-
-            if(notification.fields){
-
-            }
-        });
-    </script> --}}
-    @endif
 @endsection
