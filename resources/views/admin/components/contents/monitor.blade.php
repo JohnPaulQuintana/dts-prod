35c33e259601476276609e8bdbba897e63ed746e
abirmehedi77 <86663285+abirmehedi77@users.noreply.github.com>
2024-01-07 01:47:06 +0800

    Update monitor.blade.php

--- resources/views/admin/components/contents/monitor.blade.php
+++ resources/views/admin/components/contents/monitor.blade.php
@@ -441,7 +441,7 @@
                     const baseUrls = `${window.location.protocol}//${window.location.hostname}:${window.location.port}`;
                     var docPath = $(this).data("document-id");
                     var id = parseInt($(this).data("id"));
-                    alert(id)
+                    
                     var trkId = $(this).data("trk");
                     var purpose = $(this).data("purpose");
                     var stats = $(this).data("stats")
@@ -507,6 +507,7 @@
                         success: function(response) {
                             // Resolve the promise with the response
                            console.log(response)
+                           window.location.reload()
                         },
                         error: function(xhr, status, error) {
                             // Reject the promise with an error
